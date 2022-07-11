2468b3e417d1d118fb1ff677eff242580153ccf0
davemurdock55 <davemurdock55@gmail.com>
2022-07-10 19:23:30 -0600

    linked to those pages

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+// function newPage(...) { 
+// window.open(url, target, windowSettings); 
+// }  
+
+function newPage() {
+    window.open("");
+    opened.document.write("<html><head><title>My Title</title></head><body>test</body></html>");
+} 
